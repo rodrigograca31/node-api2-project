fa8244fb82ec7b32e3b3ab44c22325d9bb777f37
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-14 23:09:26 +0000

    Stretch 1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const server = express();
 const postsRouter = require("./routes/posts/router");
+const cors = require("cors");
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
